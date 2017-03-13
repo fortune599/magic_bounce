b15822503e4f5a995117b59414b14338e36d090a
Fortune Soleil <fsoleil@stuy.edu>
2017-03-12 23:05:14 -0400

    Update svg.js

--- svg.js
+++ svg.js
@@ -2,6 +2,13 @@ var pic = document.getElementById("vimage");
 var clr = document.getElementById("clear");
 var mvr = document.getElementById("move");
 
+var fract = document.createElementNS("http://www.w3.org/2000/svg",
+ "line");
+fract.setAttribute("x1", 250);
+fract.setAttribute("y1", 0);
+fract.setAttribute("x2", 250);
+fract.setAttribute("y1", 500);
+
 var shape = function(x, y) {
   var c = document.createElementNS("http://www.w3.org/2000/svg",
  "circle");
@@ -28,6 +35,7 @@ var shape = function(x, y) {
       c.setAttribute( "fill", "black" );
     };
   });
+  return c;
 };
 
 var atPoint = function(event){
@@ -42,13 +50,13 @@ var rayshift = function(){
   var elems = document.getElementsByTagName("circle");
   for (var i=0; i<elems.length; i++) {
     c = elems[i];
-    if (c.getAttribute("cx") >= 475){
+    if (c.getAttribute("cx") >= (475)){
       c.setAttribute("xshift", -1);
     };
     if (c.getAttribute("cx") <= 25){
       c.setAttribute("xshift", 1);
     };
-    if (c.getAttribute("cy") >= 475){
+    if (c.getAttribute("cy") >= (475)){
       c.setAttribute("yshift", -1);
     };
     if (c.getAttribute("cy") <= 25){
@@ -59,6 +67,16 @@ var rayshift = function(){
     dy = parseInt(c.getAttribute("yshift"));
     c.setAttribute("cx", parseInt(c.getAttribute("cx")) + dx);
     c.setAttribute("cy", parseInt(c.getAttribute("cy")) + dy);
+
+    if (Math.abs(c.getAttribute("cx") - 250) < 1){
+      c.setAttribute("r", c.getAttribute("r")/2);
+      var ghost = shape(c.getAttribute("cx"), c.getAttribute("cy"));
+      ghost.setAttribute("xshift", c.getAttribute("xshift") * -1);
+      ghost.setAttribute("yshift", c.getAttribute("yshift"));
+      ghost.setAttribute("r", c.getAttribute("r"));
+      ghost.setAttribute("fill", c.getAttribute("fill"));
+    };
+    if (c.getAttribute("r") <= 2) pic.removeChild(c);
   };
   intervalID = window.setInterval(rayshift, 10);
 };
